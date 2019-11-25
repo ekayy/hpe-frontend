160651fdaedf8e763d18d83acaa1ab70896ca657
Eric K Wu <ekuan.wu@gmail.com>
2019-11-25 11:30:15 -0800

    Grommet component test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import { Grommet, Box, Button, Heading } from 'grommet';
+import { Notification } from 'grommet-icons';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level="3" margin="none">
+            My App
+          </Heading>
+          <Button icon={<Notification />} onClick={() => {}} />
+        </AppBar>
+        <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align="center" justify="center">
+            app body
+          </Box>
+          <Box
+            width="medium"
+            background="light-2"
+            elevation="small"
+            align="center"
+            justify="center"
+          >
+            sidebar
+          </Box>
+        </Box>
+      </Box>
+    </Grommet>
   );
-}
+};
+
+const AppBar = props => {
+  return (
+    <Box
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      background="brand"
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      elevation="medium"
+      style={{ zIndex: '1' }}
+      {...props}
+    />
+  );
+};
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6'
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px'
+    }
+  }
+};
 
 export default App;
